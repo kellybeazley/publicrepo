2f576272523aec159148b801ba4a4d1c1087292b
Kelly Beazley <kelly_x279@hotmail.com>
2020-09-15 13:42:35 +0100

    terraform formatting

--- terraform/gcp/project-creation.tf
+++ terraform/gcp/project-creation.tf
@@ -5,16 +5,16 @@ module "gcp-project" {
   gcp_project_id  = "project-"
   # PROJECT TAGS -----------------------------------------------------------------------------------------------
   # Only hyphens (-), underscores (_), lowercase characters and numbers are allowed. International characters are allowed.
-  env        = "prod"
-  costcentre = "********"
-  cost_desc  = "***********"
-  rev_gen    = "true"
-  owner      = "kel-b"
-  age_limit  = "4-years"
-  team       = "kelly-team"
+  env             = "prod"
+  costcentre      = "********"
+  cost_desc       = "***********"
+  rev_gen         = "true"
+  owner           = "kel-b"
+  age_limit       = "4-years"
+  team            = "kelly-team"
   #-------------------------------------------------------------------------------------------------------------
-  folder_id     = "folder_id here"
-  user_email    = ["kelly@****.com"]
-  activate_apis = ["compute.googleapis.com", "container.googleapis.com", "vpcaccess.googleapis.com"]
-  budget        = "100000"
+  folder_id       = "folder_id here"
+  user_email      = ["kelly@****.com"]
+  activate_apis   = ["compute.googleapis.com", "container.googleapis.com", "vpcaccess.googleapis.com"]
+  budget          = "100000"
 }
